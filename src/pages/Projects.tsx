0649468294bdbf5f0bca894243a8451bf06414e3
Karol <66336947+karol7git@users.noreply.github.com>
2024-08-16 18:15:48 +0000

    create project card

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,85 +1,58 @@
-import { SyntheticEvent, useState } from "react";
-import gif from "../assets/gif.gif";
-import logo from "../assets/logo.png";
-import { PropTypes, Tabs, Tab, Box } from "@mui/material";
-
-interface TabPanelProps {
-  children?: React.ReactNode;
-  index: number;
-  value: number;
-}
+import img1 from "../assets/websitempire.webp";
+import img2 from "../assets/websitenoltic.jpg";
+import img3 from "../assets/websitesuso.jpg";
+import ProjectCard from "../components/ProjectCard";
 
 const projects = [
   {
-    title: "Lorem Ipsum Dolor",
-    description: "Lorem Ipsum Dolor Lorem Ipsum Dolor",
-    imgUrl: gif,
+    title: "lorem ipsum",
+    description: "lorem ipsum dolor assemt",
+    imgURL: img1,
+    siteURL: "https://flowbite.com/docs/components/card/#card-with-image",
   },
   {
-    title: "Lorem Ipsum Dolor",
-    description: "Lorem Ipsum Dolor Lorem Ipsum Dolor",
-    imgUrl: logo,
+    title: "lorem ipsum",
+    description: "lorem ipsum dolor assemt",
+    imgURL: img2,
+    siteURL: "https://flowbite.com/docs/components/card/#card-with-image",
   },
   {
-    title: "Lorem Ipsum Dolor",
-    description: "Lorem Ipsum Dolor Lorem Ipsum Dolor",
-    imgUrl: gif,
+    title: "lorem ipsum",
+    description: "lorem ipsum dolor assemt",
+    imgURL: img3,
+    siteURL: "https://flowbite.com/docs/components/card/#card-with-image",
   },
   {
-    title: "Lorem Ipsum Dolor",
-    description: "Lorem Ipsum Dolor Lorem Ipsum Dolor",
-    imgUrl: logo,
+    title: "lorem ipsum",
+    description: "lorem ipsum dolor assemt",
+    imgURL: img1,
+    siteURL: "https://flowbite.com/docs/components/card/#card-with-image",
+  },
+
+  {
+    title: "lorem ipsum",
+    description: "lorem ipsum dolor assemt",
+    imgURL: img2,
+    siteURL: "https://flowbite.com/docs/components/card/#card-with-image",
+  },
+
+  {
+    title: "lorem ipsum",
+    description: "lorem ipsum dolor assemt",
+    imgURL: img3,
+    siteURL: "https://flowbite.com/docs/components/card/#card-with-image",
   },
 ];
-function CustomTabPanel(props: TabPanelProps) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
-    </div>
-  );
-}
-function a11yProps(index: number) {
-  return {
-    id: `simple-tab-${index}`,
-    "aria-controls": `simple-tabpanel-${index}`,
-  };
-}
 const Projects = () => {
-  const [value, setValue] = useState(0);
-
-  const handleChange = (event: SyntheticEvent, newValue: number) => {
-    setValue(newValue);
-  };
   return (
-    <section className="project">
-      <h2>Projects</h2>
-      <Box sx={{ width: "100%" }}>
-        <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-          <Tabs
-            value={value}
-            onChange={handleChange}
-            aria-label="basic tabs example"
-          >
-            <Tab label="Item One" {...a11yProps(0)} />
-            <Tab label="Item Two" {...a11yProps(1)} />
-            <Tab label="Item Three" {...a11yProps(2)} />
-          </Tabs>
-        </Box>
-        <CustomTabPanel value={value} index={0}>
-          {projects.map((project, index) => {
-            return <p>{project.title}</p>;
-          })}
-        </CustomTabPanel>
-      </Box>
-    </section>
+    <>
+      <h1> Mis proyectos</h1>
+      <div className="flex flex-wrap gap-4 p-4">
+        {projects.map((project, index) => (
+          <ProjectCard key={index} {...project} />
+        ))}
+      </div>
+    </>
   );
 };
 export default Projects;
